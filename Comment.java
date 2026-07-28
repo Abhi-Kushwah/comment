9e376de11475286f5b1e28610592e89d1b27960f
Abhi-Kushwah <abhishekkushwah75094@gmail.com>
2026-07-28 23:42:46 +0530

    first commit

---
+++ Comment.java
@@ -0,0 +1,22 @@
+// there are three type of comments :-> single line comment,multiple //line comment,document comment(javadoc).
+ 
+public class Comment{
+    public static void main(String[] args){
+
+       int x=10;// variable declaration
+
+       System.out.println("Value x :"+x); 
+    }
+}
+
+//This is a single-line comment 
+
+/*
+* This is a multi-line comment
+* It can span across multiple lines
+*/
+
+/**
+* This is a documentation comment (Javadoc)
+* It is used to generate documentation for methods, classes, etc.
+*/
